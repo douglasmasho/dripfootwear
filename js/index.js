1f28da81dc9d99ed179c3ebf36f7dd7d6aa52c9f
douglasmasho <douglasmasho@gmail.com>
2020-01-26 18:05:14 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+new Glider(document.querySelector(".glider"), {
+    slidesToShow: 1,
+    dots: ".dots",
+    draggable: true,
+    arrows: {
+        prev: "#slider-prev",
+        next: "#slider-next"
+    }
+})
