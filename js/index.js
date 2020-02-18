c5a0f25251b24e7e677afdf4af9b1b6fa4a0e295
douglasmasho <douglasmasho@gmail.com>
2020-02-18 09:51:10 +0200

    Second commit
    1. Added more slides.
    2.Added a slider using glider js
    3. on the dc page, clicking the colors will change the image src(Its supposed to change th color fo the shoe)

--- js/index.js
+++ js/index.js
@@ -7,3 +7,72 @@ new Glider(document.querySelector(".glider"), {
         next: "#slider-next"
     }
 })
+
+//popup trigger
+var animatedElements1 = document.querySelectorAll(".animate1");
+
+function trigger(){
+    animatedElements1.forEach(function(element){
+       element.style.animation = "popup 1s ease-in";
+    })
+}
+
+function removeanim(){
+    animatedElements1.forEach(function(element){
+        element.style.animation = "none";
+    })
+}
+
+
+function startTimer(){
+    window.setTimeout(removeanim, 1000);
+}
+
+//slideleft trigger
+
+var animatedElements2 = document.querySelectorAll(".animate2");
+
+function trigger2(){
+    animatedElements2.forEach(function(element){
+       element.style.animation = "slideleft 1s ease-in-out";
+    })
+}
+
+function removeanim2(){
+    animatedElements2.forEach(function(element){
+        element.style.animation = "none";
+    })
+}
+
+function startTimer2(){
+    window.setTimeout(removeanim2, 1000);
+}
+
+var dots = document.querySelectorAll(".glider-dot");
+
+dots.forEach(function(element){
+    element.addEventListener("click", function(){
+        trigger(), startTimer(), trigger2(), startTimer2();
+    })
+})
+
+
+
+function changedctowhite(){
+    document.getElementById("shoe4").src = "img/dcwhite.png";
+
+}
+
+function changedctored(){
+    document.getElementById("shoe4").src = "img/dcred.png";
+}
+
+function changedctoblack(){
+    document.getElementById("shoe4").src = "img/dcblack.png";
+}
+
+function changedctocyan(){
+    document.getElementById("shoe4").src = "img/dccyan.png";
+}
+
+
