2dce7e9c3afd8b6303a951c89721db4ede138238
douglasmasho <douglasmasho@gmail.com>
2020-04-20 00:16:07 +0200

    Seventh commit
    Changed the slider, added more elements

--- js/index.js
+++ js/index.js
@@ -1,12 +1,3 @@
-new Glider(document.querySelector(".glider"), {
-    slidesToShow: 1,
-    dots: ".dots",
-    draggable: true,
-    arrows: {
-        prev: "#slider-prev",
-        next: "#slider-next"
-    }
-})
 
 //popup trigger
 var animatedElements1 = document.querySelectorAll(".animate1");
